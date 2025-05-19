b918afa6500788566ce2267aa0914daa8e36796a
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-05-19 11:17:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const mainContainer = document.querySelector(".container");
+
+for (let i = 0; i < 800; i++) {
+	mainContainer.innerHTML += `
+		<div class="square"></div>
+	`;
+}
